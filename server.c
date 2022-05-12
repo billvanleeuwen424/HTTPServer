a9c55f4a0fdb5c0ed7dd70f2de854882bec34e14
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2022-05-12 11:30:34 -0400

    copied the code over from tcpUpperServer

--- server.c
+++ server.c
@@ -1,5 +1,103 @@
 #include <server.h>
 
-int main(){
-    
+int main() {
+
+    //define address type we are looking for
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));   //zero out hints (THIS IS ABSOLOUTELY ESSENTIAL FOR SOME REASON)
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+
+    //get address
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    /*define socket*/
+    int listen_socket;
+
+    /*create socket*/
+    if((listen_socket = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol)) < 0){
+        fprintf(stderr, "socket create errror. %s\n", strerror(errno));
+        return -1;
+    }
+
+    printf("Binding socket to local address...\n");
+    /*bind*/
+    if(bind(listen_socket, bind_address->ai_addr, bind_address->ai_addrlen) < 0){
+        fprintf(stderr, "bind errror. %s\n", strerror(errno));
+        return -1;
+    }
+
+    //release bind address mem
+    freeaddrinfo(bind_address);
+
+
+    /*wait for connection*/
+    if(listen(listen_socket, 10) < 0){
+        fprintf(stderr, "listen error. %s\n", strerror(errno));
+        close(listen_socket);
+        return -1;
+    }
+
+    //create a file descriptor set, add listen socket
+    fd_set master;
+    FD_ZERO(&master);
+    FD_SET(listen_socket, &master);
+    int maxSocket = listen_socket;
+
+    printf("Waiting for connections...\n");
+
+    int client_socket;
+
+    while(1){
+        
+        struct sockaddr_storage client;
+        socklen_t clientlen = sizeof(client);
+
+        if((client_socket = accept(listen_socket, (struct sockaddr*) &client, &clientlen)) < 0){
+            fprintf(stderr, "Error accepting connection. %s\n", strerror(errno));
+            close(client_socket);
+            close(listen_socket);
+            return -1;
+        }
+
+        //log connection to console
+        char addrstr[100];
+        getnameinfo((struct sockaddr*)&client, clientlen, addrstr, sizeof(addrstr), 0,0,NI_NUMERICHOST);
+        printf("Connection from: %s\n", addrstr);
+
+
+        int pid = fork();
+
+        if(pid == 0){
+
+            while(1){
+                
+                close(listen_socket);
+
+                //recieve request from client
+                char request[1024];
+                int bytes_recieved = recv(client_socket, request, 1024, 0);
+
+                //close client if nothing recieved
+                if(bytes_recieved <= 1){
+                    close(client_socket);
+                    exit(0);
+                }
+
+                //convert request string to uppercase and send
+                for (int i = 0; i < bytes_recieved; i++)
+                {
+                    request[i] = toupper(request[i]);
+                }
+
+                send(client_socket, request, bytes_recieved, 0);
+            }
+        }   //if pid == 0
+    }   //while 1
+
+    close(listen_socket);
+    return 0;
 }
